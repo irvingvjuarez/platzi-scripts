7fb54a77723aa7bc388a7918c4094b09dc74f69c
irvingvjuarez <Irving.Juarez@thomsonreuters.com>
2024-10-27 23:59:59 -0600

    modifying index.js

--- webapp/index.js
+++ webapp/index.js
@@ -5,10 +5,21 @@ const app = new Koa();
 const router = new Router();
 const sum = require('./sum');
 
-router.get('/add/:a/:b', (ctx, next) => {
-  const result = sum(parseFloat(ctx.params.a), parseFloat(ctx.params.b));
-  return ctx.body = { result };
-});
+router
+  .get('/', (ctx, next) => {
+    return ctx.body = { message: 'Hello World' };
+  })
+  .get('/add', (ctx, next) => {
+    return ctx.body = { message: 'Add endpoint, dummy one' };
+  })
+  .get('/add/:a', (ctx, next) => {
+    const value = ctx.params.a;
+    return ctx.body = { value };
+  })
+  .get('/add/:a/:b', (ctx, next) => {
+    const result = sum(parseFloat(ctx.params.a), parseFloat(ctx.params.b));
+    return ctx.body = { result };
+  });
 
 app
   .use(router.routes())
